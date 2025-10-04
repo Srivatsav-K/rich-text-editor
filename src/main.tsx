9f9b98c72395922f62ef0397851c6ceb834ec20e
Srivatsav K <104587732+Srivatsav-K@users.noreply.github.com>
2025-10-04 18:42:02 +0530

    Add ThemeProvider component for theme management

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./styles/editor.css";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
